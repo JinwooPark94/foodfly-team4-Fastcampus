21d1dca9fe22fb6a70bfa841cc56607a2f717e69
simmh <simmh17@gamil.com>
2018-04-05 14:04:25 +0900

    foodlist markup #1

--- src/app/foodfly/components/restaurant/foodlist/foodlist.component.ts
+++ src/app/foodfly/components/restaurant/foodlist/foodlist.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./foodlist.component.css']
 })
 export class FoodlistComponent implements OnInit {
+  items = [];
+  item;
 
-  constructor() { }
+  constructor() { 
+    this.item = {
+      name: '스노우폭스 뱅뱅점',
+      category: ['도시락', '간편', '분식'],
+      minPrice: 15000,
+      flyTip: 3000,
+      distance: 0.8,
+      images: [
+        'https://fakeimg.pl/230x304/?text=food one',
+        'https://fakeimg.pl/230x304/?text=food two',
+      ]
+    };
+  }
 
   ngOnInit() {
+    this.itemRepeat();
+  }
+
+  itemRepeat() {
+    const repeatNum = 12;
+    for (let i = 0; i < repeatNum; i++) {
+      this.items = [...this.items, this.item];
+    }
   }
 
 }
