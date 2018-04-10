48475c48ed9ddb6c23c67ece27b20bdb89844339
simmh <simmh17@gamil.com>
2018-04-10 16:41:48 +0900

    edit main component

--- src/app/foodfly/components/main/main.module.ts
+++ src/app/foodfly/components/main/main.module.ts
@@ -1,26 +1,36 @@
-// Common 모듈
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShareModule } from '../../shared/share.module';
 
-// Route Module
-import { MainRoutingModule } from './main-routing.module';
-
-// Component main and pagenotfound
 import { MainComponent } from './main.component';
-import { NotfoundpageComponent } from '../notfoundpage/notfoundpage.component';
+
+// thiyrd-part
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto'
+};
+
 
 @NgModule({
-  declarations: [
-    MainComponent,
-    NotfoundpageComponent
-  ],
   imports: [
-    CommonModule, ShareModule, MainRoutingModule
+    CommonModule,
+    ShareModule,
+    // thiyrd-part
+    SwiperModule,
   ],
-  exports: [
-    MainComponent,
-    NotfoundpageComponent
-  ]
+  providers: [
+    // thiyrd-part
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }
+  ],
+  declarations: [ MainComponent ],
+  exports: [ MainComponent ]
+
 })
 export class MainModule { }
