68f1b4b90df8694a699ef41361f14cecf6b7798c
simmh <simmh17@gamil.com>
2018-04-10 17:31:53 +0900

    edit foodlist component

--- src/app/foodfly/components/restaurant/foodlist/foodlist.component.ts
+++ src/app/foodfly/components/restaurant/foodlist/foodlist.component.ts
@@ -1,37 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'foodfly-foodlist',
   templateUrl: './foodlist.component.html',
-  styleUrls: ['./foodlist.component.css']
+  styleUrls: ['./foodlist.component.css'],
 })
+
 export class FoodlistComponent implements OnInit {
+  url = 'http://localhost:3000/restaurant';
+  foodflyDB;
+
   items = [];
   item;
 
-  constructor() { 
-    this.item = {
-      name: '스노우폭스 뱅뱅점',
-      category: ['도시락', '간편', '분식'],
-      minPrice: 15000,
-      flyTip: 3000,
-      distance: 0.8,
-      images: [
-        'https://fakeimg.pl/230x304/?text=food one',
-        'https://fakeimg.pl/230x304/?text=food two',
-      ]
-    };
+  over: boolean[];
+  scrollTopVisble: boolean;
+
+  filter = ['거리순', '인기순', '배달팁 순', '최소 주문 금액 순'];
+  selectedFilter = '';
+
+  state = 'smaller';
+
+  constructor(public http: HttpClient) {
+    this.scrollTopVisble = false;
   }
 
   ngOnInit() {
     this.itemRepeat();
+    console.log();
+
+    this.http.get(this.url)
+      // 요청 결과를 프로퍼티에 할당
+      .subscribe(data => {
+        this.foodflyDB = data;
+        console.log('[data]', data);
+
+        this.over = new Array(this.foodflyDB.length);
+        this.over.fill(false);
+      });
+
+    console.log(window);
   }
+
 
   itemRepeat() {
     const repeatNum = 12;
     for (let i = 0; i < repeatNum; i++) {
       this.items = [...this.items, this.item];
     }
+    console.log(this.items);
   }
 
+
+  scrollTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+    console.log('top');
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // we'll do some stuff here when the window is scrolled
+    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+  if (number > 100) {
+      this.scrollTopVisble = true;
+    } else if (this.scrollTopVisble && number < 10) {
+      this.scrollTopVisble = false;
+    }
+  }
+
+
+
+  selectFilter(e) {
+    console.log('click', e);
+  }
 }
