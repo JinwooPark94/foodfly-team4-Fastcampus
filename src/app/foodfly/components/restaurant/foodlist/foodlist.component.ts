5a015c70a2f7276826138a41bc98753329d93bec
simmh <simmh17@gamil.com>
2018-04-11 12:56:58 +0900

    edite foodlist pagination

--- src/app/foodfly/components/restaurant/foodlist/foodlist.component.ts
+++ src/app/foodfly/components/restaurant/foodlist/foodlist.component.ts
@@ -16,7 +16,7 @@ export class FoodlistComponent implements OnInit {
   over: boolean[];
   scrollTopVisble: boolean;
 
-  pageItemNum = 8;
+  pageItemNum = 3;
   scrollMessage;
 
   filter = ['거리순', '인기순', '배달팁 순', '최소 주문 금액 순'];
@@ -50,7 +50,6 @@ export class FoodlistComponent implements OnInit {
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    // we'll do some stuff here when the window is scrolled
     const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
     if (number > 100) {
         this.scrollTopVisble = true;
@@ -58,12 +57,13 @@ export class FoodlistComponent implements OnInit {
         this.scrollTopVisble = false;
       }
 
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 50) {
       this.pagination();
     }
   }
 
   pagination() {
+    //
     if (!this.items) {
       this.items = this.foodflyDB.slice(0, this.pageItemNum);
       console.log('[items 0 ]');
@@ -71,7 +71,19 @@ export class FoodlistComponent implements OnInit {
     }
 
     const arr = this.foodflyDB.slice(this.items.length, this.items.length + this.pageItemNum);
-    console.log('현재 item', this.items, 'slice', this.items.length + this.pageItemNum);
+    // const restItem = this.foodflyDB.length - this.items.length;
+
+    console.log(
+      'db imte', this.foodflyDB.length,
+      '현재 item', this.items,
+      'slice', this.items.length + this.pageItemNum,
+      // 'rest item', restItem
+    );
+
+    // if (restItem < this.pageItemNum && restItem > 1) {
+    //   console.log('더있음');
+    // }
+
     if (this.foodflyDB.length === this.items.length) {
       this.scrollMessage = '마지막 페이지 입니다.';
       return;
