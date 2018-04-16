198cb056ba2d05a08d566b8e96b12ddaa0a8edf9
JinwooPark94 <wlsdntus4@gmail.com>
2018-04-16 23:33:23 +0900

    edit cart function and icon

--- src/app/foodfly/core/core.module.ts
+++ src/app/foodfly/core/core.module.ts
@@ -6,6 +6,7 @@ import { JwtHelper } from 'angular2-jwt';
 import { LoginService } from './services/login.service';
 import { SocialLoginService } from './services/social-login.service';
 import { ToastService } from './services/toast.service';
+import { FoodorderService } from './services/foodorder.service';
 
 import { TokenGuard } from './guards/token.guard';
 
@@ -16,6 +17,7 @@ import { ToastPipe } from './pipes/toast.pipe';
     JwtHelper,
     LoginService,
     SocialLoginService,
+    FoodorderService,
     ToastService,
     TokenGuard
   ],
--- src/app/foodfly/core/services/foodorder.service.ts
+++ src/app/foodfly/core/services/foodorder.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { OrderList } from '../../core/interface/foodorder.interface';
 
-
 @Injectable()
 export class FoodorderService {
 
@@ -11,48 +10,23 @@ export class FoodorderService {
 
   minOrderPrice = 30000;
 
+
+  cartData;
+
   constructor() { }
 
-  checkOrderList(orderedItem: OrderList) {
-    const MatchId = this.orderlist.find(item => item.id === orderedItem.id);
+  // 장바구니 list에 메뉴가 들어가 있는지 확인 후 개수 리턴
+  getSessionData() {
+    this.cartData = JSON.parse(sessionStorage.getItem('sessionStorage-cart'));
 
-    if (MatchId) {
-      return this.addAmount(orderedItem.id);
-    } else {
-      return this.addOrder(orderedItem);
-    }
+    if (!this.cartData) { return 0; }
+    return this.cartData.orderList.length;
   }
 
-  addOrder(orderedItem: OrderList) {
-    this.orderlist = [...this.orderlist,
-    { id: orderedItem.id, name: orderedItem.name, price: orderedItem.price, amount: orderedItem.amount }];
-  }
-
-  addAmount(id: number) {
-    this.orderlist = this.orderlist.map(order => order.id === id ? Object.assign({}, order, { amount: order.amount + 1 }) : order);
-  }
-
-  minusAmount(order: OrderList) {
-    if (order.amount <= 1) {
-      return;
-    } else {
-      this.orderlist = this.orderlist.map(orderedItem => orderedItem.id === order.id ?
-        Object.assign({}, orderedItem, { amount: orderedItem.amount - 1 }) : orderedItem);
-    }
-  }
-
-  removeToOrder(id: number) {
-    this.orderlist = this.orderlist.filter(order => order.id !== id);
-  }
-
-  orderSumCulator() {
-    const orderMidSum = this.orderlist.map(orderedItem => orderedItem.price * orderedItem.amount);
-
-    if (this.orderlist.length) {
-      return this.orderSum = orderMidSum.reduce((accumulator, currentValue) => accumulator + currentValue);
-    } else {
-      return this.orderSum = 0;
-    }
+  // sessionStorage에 있는 메뉴가 있을 시 값 리턴
+  getOrderlistData() {
+    if (!this.cartData.orderList) { return 0; }
+    return this.cartData.orderList;
   }
 
 }
