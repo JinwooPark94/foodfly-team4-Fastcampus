0edead166bcb82bb13b63dec4fb13f01b22acb21
austinpark420 <austin.park420@gmail.com>
2018-04-20 14:42:09 +0900

    edit foododer accodion and totalsum

--- src/app/foodfly/components/restaurant/foodorder/foodorder.component.ts
+++ src/app/foodfly/components/restaurant/foodorder/foodorder.component.ts
@@ -43,11 +43,10 @@ export class FoodorderComponent implements OnInit, DoCheck, OnDestroy {
   apiUrl = `${environment.apiUrl}`;
   restaurantDB: any = [];
   menuCategories: any = [];
+  toggle: boolean[];
 
   navItems: string[] = ['메뉴', '정보', '리뷰'];
   selectedItem: string;
-
-  toggleMenu = false;
 
   routerSubscriber: Subscription;
 
@@ -67,35 +66,39 @@ export class FoodorderComponent implements OnInit, DoCheck, OnDestroy {
         this.foodorderService.orderlist = this.foodorderService.getOrderlistData();
         this.getRestaurant(this.foodorderService.restaurantPk);
       });
-    this.selectedItem = this.navItems[0];
-  }
-
-  ngOnDestroy() {
-    this.routerSubscriber.unsubscribe();
-  }
-
-  // orderlist 배열을 확인하여 값이 바뀌면 실행
-  ngDoCheck() {
-    const changes = this.differs.find(this.foodorderService.orderlist);
-    if (changes) {
-      // this.restaurantDB.name : 레스토랑 이름
-      // session에 데이터 저장
-      this.foodorderService.setFoodOrderStorage(this.restaurantDB.name);
+      this.selectedItem = this.navItems[0];
     }
-  }
 
-  getRestaurant(pk) {
-    this.preloadService.show();
-    this.http.get(`${this.apiUrl}/restaurants/${pk}`)
-    .subscribe( data => {
-      this.restaurantDB = data;
-      this.menuCategories = data['menuCategories'];
-      console.log('[get restaurant]', data['menuCategories'][0]['menus']);
-      this.preloadService.hide();
+    ngOnDestroy() {
+      this.routerSubscriber.unsubscribe();
+    }
+
+    // orderlist 배열을 확인하여 값이 바뀌면 실행
+    ngDoCheck() {
+      const changes = this.differs.find(this.foodorderService.orderlist);
+      if (changes) {
+        // this.restaurantDB.name : 레스토랑 이름
+        // session에 데이터 저장
+        this.foodorderService.setFoodOrderStorage(this.restaurantDB.name);
+      }
+    }
+
+    getRestaurant(pk) {
+      this.preloadService.show();
+      this.http.get(`${this.apiUrl}/restaurants/${pk}`)
+      .subscribe( data => {
+        this.restaurantDB = data;
+        this.menuCategories = data['menuCategories'];
+        console.log('[get restaurant]', data['menuCategories'][0]['menus']);
+        this.preloadService.hide();
+        this.toggle = new Array(this.menuCategories.length);
+        this.toggle.fill(true);
+        this.toggle[0] = false;
     });
   }
 
   changeNavItem(navItem: string) {
     this.selectedItem = navItem;
   }
+
 }
